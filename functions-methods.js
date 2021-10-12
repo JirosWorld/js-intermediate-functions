e7af6e19b147ec59a19a3878a1313eab451a86a3
jolarti <jolarti@gmail.com>
2021-10-12 17:56:37 +0200

    opdracht 1 + comments

--- functions-methods.js
+++ functions-methods.js
@@ -9,7 +9,19 @@
 // getEmailDomain("t.mellink@novi.nl") geeft novi.nl
 // getEmailDomain("a.wiersma@outlook.com") geeft outlook.com
 
+// TESTWAARDEN:
+// let emailAddress1 = "n.eeken@novi-education.nl";
+// let emailAddress1 = "a.wiersma@outlook.com";
+console.log("Opdracht  1:");
 
+function getEmailDomain(emailAddress1) {
+    const domainCut = emailAddress1.indexOf('@');
+    const domainName = emailAddress1.substring(domainCut + 1);
+    // TEST LOG: console.log(domainName);
+    return domainName;
+}
+getEmailDomain("t.mellink@novi.nl");
+console.log(getEmailDomain("t.mellink@novi.nl"));
 
 
 /* Opdracht  2 */
@@ -20,7 +32,28 @@
 // typeOfEmail("novi.nlaapjesk@outlook.com") geeft geeft "Extern" <-- deze moet het ook doen!
 // typeOfEmail("a.wiersma@outlook.com") geeft "Extern"
 
+// TEST LOG
+// let emailaddress2 = "t.mellink@novi.nl";
+console.log("Opdracht  2:");
 
+function typeOfEmail(emailaddress2) {
+    // eigenlijk willen we niet het hele emailadres, maar alleen het stuk erna/domein checken:
+    // dus: de oude functie uit opdracht 1 hergebruiken
+    getEmailDomain(emailaddress2);
+
+    // TEST LOG: console.log("de functie is gestart:" + emailaddress2);
+
+    if (getEmailDomain(emailaddress2).includes("novi.nl")) {
+        console.log("Deze persoon met maildomein '" + getEmailDomain(emailaddress2) + "' is een: 'Medewerker'");
+    }
+    if (getEmailDomain(emailaddress2).includes("novi-education.nl")) {
+        console.log("Deze persoon met maildomein '" + getEmailDomain(emailaddress2) + "' is een: 'Student'");
+    }
+    if (getEmailDomain(emailaddress2).includes("outlook.com")) {
+        console.log("Deze persoon met maildomein '" + getEmailDomain(emailaddress2) + "' is: 'Extern'");
+    }
+}
+typeOfEmail("novi.nlaapjesk@outlook.com");
 
 /* Opdracht  3 */
 // Schrijf een functie genaamd checkEmailValidity, die een emailadres verwacht en checkt of het emailadres valide is. De functie returned true of false, afhankelijk van de uitkomst.
@@ -34,3 +67,37 @@
 // checkEmailValidity("n.eekenanovi.nl") geeft false - want geen @
 // checkEmailValidity("n.eeken@novinl.") geeft false - want de punt mag niet als laatst
 // checkEmailValidity("tessmellink@novi,nl") geeft false - want er staat een komma in
+
+console.log("Opdracht  3:");
+
+function checkEmailValidity(emailaddress3) {
+    // nu willen we weer het hele emailadres checken, dus incl. domein
+
+    // TEST LOG: console.log("de functie is gestart:" + emailaddress3);
+
+    if (!(emailaddress3.includes("@"))) {
+        console.log("Dit adres: '" + emailaddress3 + "' mist een apenstaartje.");
+        console.log(false);
+        return false;
+    }
+    if (emailaddress3.includes(",")) {
+        console.log("Dit adres: '" + emailaddress3 + "' is fout want er zit een komma in.");
+        console.log(false);
+        return false;
+    }
+    //checken of de punt op het eind staat
+    let checkEmailaddress3 = emailaddress3.charAt(emailaddress3.length-1);
+
+    if (checkEmailaddress3 == ".") {
+        console.log("Dit adres: '" + emailaddress3 + "' is fout want er staat een punt op het eind.");
+        console.log(false);
+        return false;
+    }
+    //    de returns stoppen de functie dus: de correcte statement moet helemaal op het eind
+    if ((emailaddress3.includes("@"))) {
+        console.log("Dit adres: '" + emailaddress3 + "' is correct.");
+        console.log(true);
+        return true;
+    }
+}
+checkEmailValidity("tessmellink@novi,nl");
